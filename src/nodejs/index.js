69cadd630ac86d00708d918d6771c825991d8a97
Manh Le <quang.manh@gmail.com>
2017-11-21 23:56:51 -0600

    Finish Pack at saveContact

--- src/nodejs/index.js
+++ src/nodejs/index.js
@@ -1,43 +1,81 @@
-//inPort
-//makeContact
-//onVibration
+//inPorts:[]
+//saveContact()
+//onVibration()
 //
-var X ={};
+var X = {};
 
 class Unit {
-    constructor(props){
-        this.__={
-            props:props||{}
+    constructor(props) {
+        this.__ = {
+            props: props || {}
         }
     }
-    makeContact(unit,atMedium){
+    saveContact(unit, atMedium) {}
+
+    onVibration(fromUnit, atMedium, signal) {}
+
+    createProp(name, defVal, get, set) {
+        Unit.createProp(this, name, defVal, get, set, this.__.props)
     }
 
-    onVibration(fromUnit,atMedium,signal){
-    }
+    static createProp(O, name, def, getFx, setFx, storage) {
+        getFx ? 1 : getFx = Unit.CONST.defaultGet;
+        setFx ? 1 : setFx = Unit.CONST.defaultSet;
 
-    createProp(name,defVal,get,set){
-        Unit.createProp(this,name,defVal,get,set,this.__.props)
-    }
-
-    static createProp(O,name,def,getFx,setFx,storage){
-        getFx?1:getFx=function(name,store,defVal){
-            store.hasOwnproperty(name)?1:store[name] = defVal;
-            return store[name];
-        }
-        setFx?1:setFx=function(name,store,val){
-            store[name] = val;
-        }
-        Object.defineProperty(O,name,{
-            get:function(){
-                getFx.call(this,name,storage,defVal);
+        Object.defineProperty(O, name, {
+            get: function () {
+                getFx.call(this, name, storage, defVal);
             },
-            set:function(val){
-                setFx.call(this,name,storage,val)
+            set: function (val) {
+                setFx.call(this, name, storage, val)
             }
         })
+    }
+
+    static CONST = {
+        defaultGet(name, store, defVal) {
+            store.hasOwnproperty(name) ? 1 : this[name] = defVal;
+            return store[name];
+        },
+        defaultSet(name, store, val) {
+            store[name] = val;
+        }
+    }
+}
+
+export default Unit;
+
+
+class Pack extends Unit {
+    constructor(props) {
+        super(props);
+        this.__.ports = {};
+        this.__.contacts = new Map();
+
+        this.createProp("staged", true);
+        this.createProp("fx", function () {})
+        this.createProp("inPorts", []);
+        this.createProp("props", null, function () {
+            return this.__.props;
+        }, function () {})
+
+        this.createProp("ports", null, function () {
+            return this.__.ports
+        }, function () {});
+
+
+    }
+
+    saveContact(unit, atMedium) {
+        const inPorts = this.inPorts;
+        const {contacts} = this.__;
+                
+    }
+
+    onVibration(fromUnit, atMedium, signal){
+        
     }
 }
 
 
-
+export default Pack;
