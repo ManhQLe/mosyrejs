c7820654617c673d81a25c522159edc9c3689c50
Manh Le <quang.manh@gmail.com>
2018-02-14 23:24:56 -0600

    VerifierClayUsage complete

---
+++ src/tests/nb_SClay.js
@@ -0,0 +1,70 @@
+const {SClay,RClay,Conduit} = require("../mosyrejs")
+const VerifierClay = require("./VerifierClay")
+const BlockClay = require("./BlockClay")
+
+const Verifier = new VerifierClay({
+    actLogic:(testCase,center)=>{
+        center.A = testCase[0];
+        center.B = testCase[1];
+    },
+    verifyLogic:(testCase,center)=>{
+        VerifierClay.Assert(center.RESULT,testCase[2]);
+    },
+    testSet:[
+        [2,3,24],
+        [1,3,12],
+        [5,1,20],
+        [3,6,72]
+    ],
+    connectPoints:["RESULT"]
+})
+
+
+class Adder extends RClay{
+    constructor(agr){
+        super(agr);
+        this.connectPoints=["A","B"],
+        this.staged = true;
+    }
+
+    onResponse(){
+        const center = this.getCenter();
+        center.C = center.A + center.B
+    }
+}
+
+class Mult extends RClay{
+    constructor(agr){
+        super(agr);
+        this.connectPoints=["A","B"]
+        this.staged = true;
+    }
+
+    onResponse(){
+        const center = this.getCenter();
+        center.C = center.A * center.B
+    }
+}
+
+const Add1 = new Adder()
+const Add2 = new Adder()
+const Mul = new Mult()
+
+Conduit.createLink(Add1,"C",Mul,"A");
+Conduit.createLink(Add2,"C",Mul,"B");
+
+
+const SClaySubject = new SClay({
+    layoutMap:[
+        ["A",Add1,"A"],
+        ["A",Add1,"B"],
+        ["B",Add2,"A"],
+        ["B",Add2,"B"],
+        ["R",Mul,"C"]
+    ]    
+})
+
+Conduit.createLink(SClaySubject,"A",Verifier,"A");
+Conduit.createLink(SClaySubject,"B",Verifier,"B");
+Conduit.createLink(SClaySubject,"R",Verifier,"RESULT");
+Conduit.createLink(BlockClay,"EXIT",Verifier,VerifierClay.OutPoint)
