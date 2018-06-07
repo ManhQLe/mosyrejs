8e5b5fc96d440b9d9f1b8b214aee460574c1a358
Manh Le <quang.manh@gmail.com>
2018-06-06 23:24:50 -0500

    Added browser lib

---
+++ build/browser.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const Clay = require('./Clay');
+const Conduit = require('./Conduit');
+const RClay = require('./RClay');
+const AttribClay = require('./AttribClay')
+const SClay = require('./SClay')
+mosyrejs = {
+    Clay,
+    Conduit,
+    RClay,    
+    AttribClay,
+    SClay
+}
